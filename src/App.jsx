0faa6df30a4acdef0786e42643af94baa96058f3
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2026-02-04 15:26:35 -0300

    refactor: Three columns layout to make the learning process easier (with visual feedback for React controlled data)

--- src/App.jsx
+++ src/App.jsx
@@ -18,29 +18,53 @@ function App() {
 
   return (
     <>
-      <ul>
-        <li>Nome completo - input text</li>
-        <li>E-mail - input email</li>
-        <li>Senha - input password</li>
-        <li>Data de nascimento - input date</li>
-        <li>Cargo desejado - select multiple</li>
-        <li>Senioridade - select simples</li>
-        <li>Modelo de trabalho - input checkbox</li>
-        <li>Tipo de contrato - input radio</li>
-        <li>Apresentacao profissional - textarea</li>
-        <li>Aceito os termos de uso - input checkbox</li>
-      </ul>
+      <div style={{ display: "flex" }}>
+        <div>
+          <p>Mapeamento de tipo</p>
+          <ul>
+            <li>Nome completo - input text</li>
+            <li>E-mail - input email</li>
+            <li>Senha - input password</li>
+            <li>Data de nascimento - input date</li>
+            <li>Cargo desejado - select multiple</li>
+            <li>Senioridade - select simples</li>
+            <li>Modelo de trabalho - input checkbox</li>
+            <li>Tipo de contrato - input radio</li>
+            <li>Apresentacao profissional - textarea</li>
+            <li>Aceito os termos de uso - input checkbox</li>
+          </ul>
+        </div>
 
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Nome completo</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={profile.name}
-          onChange={handleChange}
-        />
-      </form>
+        <div>
+          <p>Formulario</p>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Nome completo</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={profile.name}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+
+        <div>
+          <p>Feedback visual em tempo real</p>
+          <ul>
+            <li>Nome completo: {profile.name}</li>
+            <li>E-mail: {}</li>
+            <li>Senha: {}</li>
+            <li>Data de nascimento: {}</li>
+            <li>Cargo desejado: {}</li>
+            <li>Senioridade: {}</li>
+            <li>Modelo de trabalho: {}</li>
+            <li>Tipo de contrato: {}</li>
+            <li>Apresentacao profissional: {}</li>
+            <li>Aceito os termos de uso: {}</li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
